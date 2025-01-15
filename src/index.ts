42d922573b3ccec68f865f58128f55c141697103
Aaron Goulden <aarong11@gmail.com>
2025-01-15 12:12:32 +0000

    Added github actions

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,17 @@ import path from 'path';
 
 async function main() {
   const filePath = path.resolve(__dirname, '../fixtures/input.png');
-  const imageAdapter = await new JimpAdapter({ 
-    imageMetadata: { format: 'png', size: 0, channels: ['r', 'g', 'b', 'a'] }, 
-    width: 0, 
-    height: 0, 
-    pixelData: [] 
+  const imageAdapter = await new JimpAdapter({
+    imageMetadata: { format: 'png', size: 0, channels: ['r', 'g', 'b', 'a'] },
+    width: 0,
+    height: 0,
+    pixelData: [],
   }).fromFile(filePath);
 
   const cellManager = new CellManager();
 
   const cells: Cell[] = await cellManager.partitionCellsFromImage(imageAdapter);
-  for(let i = 0; i < cells.length; i++) {
+  for (let i = 0; i < cells.length; i++) {
     async function test() {
       const cell = cells[i];
       console.log(`
